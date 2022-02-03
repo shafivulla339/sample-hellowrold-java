235f1013090021450d887723620c9ecbccb6a655
san39 <87767263+san39@users.noreply.github.com>
2022-02-03 17:08:17 +0530

    Update Application.java

--- src/main/java/com/tomgregory/Application.java
+++ src/main/java/com/tomgregory/Application.java
@@ -1,7 +1,5 @@
-package com.tomgregory;
-
-public class Application {
-    public static void main(String[] args) {
-        System.out.println("Gradle GitHub actions 4tw!");
-    }
-}
+public class HelloWorld {
+	  public static void main(String[] args) {
+	    System.out.println("Hello, World");
+	 }
+	}
